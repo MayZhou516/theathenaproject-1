be446ca97199317b7e44e038f77fa32e6830bdf7
Lucy <ll7@princeton.edu>
2017-07-14 22:16:47 -0700

    Made database

--- athena/models.py
+++ athena/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+'''
+After making changes make sure you go to theathenaproject/athena and run
+python manage.py makemigrations
+python manage.py migrate
+'''
+
+class Student(models.Model):
+    name = models.CharField(max_length=80)
+    skill_add = models.IntegerField()
+    skill_sub = models.IntegerField()
+    skill_mul = models.IntegerField()
+    skill_div = models.IntegerField()
+
+class History(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    performed_add = models.IntegerField()
+    performed_sub = models.IntegerField()
+    performed_mul = models.IntegerField()
+    performed_div = models.IntegerField()
+    date = models.DateTimeField()
