175405297c33dd192dfe64baab8a31db4a4cf0c3
Lucy Lin <ll7@princeton.edu>
2017-07-15 07:20:24 -0700

    fix import problems and bugs

--- athena/generate_image.py
+++ athena/generate_image.py
@@ -1,12 +1,13 @@
 from PIL import Image, ImageFont, ImageDraw
 from django.contrib.staticfiles.templatetags.staticfiles import static
+import os
 
+STATIC_DIRECTORY = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
 
 WIDTH, HEIGHT = 612, 792
 #im = Image.new('RGB', (WIDTH, HEIGHT), 'white')
-url = static('athena/img/template.png')
-print(url)
-im = Image.open(url)
+
+im = Image.open(STATIC_DIRECTORY + '/athena/img/template.png')
 draw = ImageDraw.Draw(im)
 FONT_SIZE = 24
 font = ImageFont.truetype('Arial.ttf', FONT_SIZE)
@@ -14,8 +15,9 @@ large_font = ImageFont.truetype('Arial.ttf', int(FONT_SIZE*1.8))
 
 ROWS = 3
 def make_homework(name, problems):
+	print(problems)
 	draw.text((WIDTH*.12, HEIGHT*.06), name, (0,0,0), large_font)
-	for num, problem in enumerate(problems):
+	for num, problem in enumerate(list(problems[0])):
 		x,y = num%ROWS, num//ROWS
 		#x_pixel = WIDTH*(.206+.255*x) - (FONT_SIZE*len(problem)/2*1.04)
 		#y_pixel = HEIGHT*(.175+.1385*y)
@@ -23,6 +25,7 @@ def make_homework(name, problems):
 		y_pixel = HEIGHT*(.15 + .1548*y)
 		if y == 0:
 			y_pixel += HEIGHT * 0.003
+		print(problem)
 		draw.text((x_pixel, y_pixel), problem, (0,0,0), font=font)
 	#im.show()
 	im.save(name.replace(' ','')+'.png')
--- athena/urls.py
+++ athena/urls.py
@@ -1,8 +1,5 @@
 from django.conf.urls import url
-
 from . import views
-
-import create_problems
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
