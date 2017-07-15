e3b7c36301a0679136e5386b7d25cc2b422309bc
Lucy <ll7@princeton.edu>
2017-07-15 06:13:46 -0700

    Image recognition is actually working ok

--- athena/generate_image.py
+++ athena/generate_image.py
@@ -1,23 +1,28 @@
 from PIL import Image, ImageFont, ImageDraw
 
 
-WIDTH, HEIGHT = 850, 1100
+WIDTH, HEIGHT = 612, 792
 #im = Image.new('RGB', (WIDTH, HEIGHT), 'white')
-im = Image.open('homework_template/Slide1.png')
+im = Image.open('template.png')
 draw = ImageDraw.Draw(im)
-FONT_SIZE = 26
+FONT_SIZE = 24
 font = ImageFont.truetype('Arial.ttf', FONT_SIZE)
 large_font = ImageFont.truetype('Arial.ttf', int(FONT_SIZE*1.8))
 
 ROWS = 3
-#problems = ['5+2=']*15
 def make_homework(name, problems):
-	draw.text((WIDTH*.1, HEIGHT*.05), name, (0,0,0), large_font)
+	draw.text((WIDTH*.12, HEIGHT*.06), name, (0,0,0), large_font)
 	for num, problem in enumerate(problems):
 		x,y = num%ROWS, num//ROWS
-		x_pixel = WIDTH*(.206+.255*x) - (FONT_SIZE*len(problem)/2*1.04)
-		y_pixel = HEIGHT*(.175+.1385*y)
+		#x_pixel = WIDTH*(.206+.255*x) - (FONT_SIZE*len(problem)/2*1.04)
+		#y_pixel = HEIGHT*(.175+.1385*y)
+		x_pixel = WIDTH*(.25 + .25*x) - (FONT_SIZE*len(problem)/2*.5)
+		y_pixel = HEIGHT*(.15 + .1548*y)
+		if y == 0:
+			y_pixel += HEIGHT * 0.003
 		draw.text((x_pixel, y_pixel), problem, (0,0,0), font=font)
+	im.show()
 	im.save(name.replace(' ','')+'.png')
 
-#make_homework('lulu', problems)
+problems = ['5+2', '55+2', '55+22']*5
+make_homework('Lulu', problems)
--- athena/parse_image.py
+++ athena/parse_image.py
@@ -1,22 +1,32 @@
 from PIL import Image
 import pytesseract
+import performRecognition
+import numpy as np
+import time
 
-NUM_PROBLEMS = 3
+NUM_PROBLEMS = 6
 ROWS = 3
-WIDTH, HEIGHT = 720, 960
-BOX_WIDTH, BOX_HEIGHT = 30, 33 # I dunno lol
+WIDTH, HEIGHT = 612, 792
+BOX_WIDTH, BOX_HEIGHT = 80, 38  # I dunno lol
 
 
 def parse_attempt(im, num):
 	x,y = num%ROWS, num//ROWS
-	x_pixel = WIDTH*(.206+.29*x)
-	y_pixel = HEIGHT*(.271+.1387*y)
+	#x_pixel = WIDTH*(.20+.3*x)
+	#y_pixel = HEIGHT*(.271+.159*y)
+	x_pixel = WIDTH*(.25 + .25*x)
+	y_pixel = HEIGHT*(.2272 + .1548*y)
+	if y == 0:
+		print('hi')
+		y_pixel += HEIGHT * 0.003
 	print(num, x_pixel, y_pixel)
 	sub = im.crop((x_pixel-BOX_WIDTH/2, y_pixel-BOX_HEIGHT/2, x_pixel+BOX_WIDTH/2, y_pixel+BOX_HEIGHT/2))
 	sub.show()
-	if num == 2:
-		sub.save('three.png')
-	text = pytesseract.image_to_string(sub)
+	#if num == 2:
+	#	sub.save('three.png')
+	#text = pytesseract.image_to_string(sub)
+	text = performRecognition.readNum(np.array(sub))
+	print(text)
 	return text.strip()
 
 
@@ -24,5 +34,5 @@ def parse_image(filename):
 	im = Image.open(filename)
 	return [parse_attempt(im, num) for num in range(NUM_PROBLEMS)]
 
-print(parse_image('lulu_done.png'))
-print('<'+pytesseract.image_to_string(Image.open('three.png'))+'>')
+print(parse_image('template_done.png'))
+#print('<'+pytesseract.image_to_string(Image.open('three.png'))+'>')
